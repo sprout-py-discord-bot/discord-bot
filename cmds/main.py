f5c483bc2e732e2d568bea823e3e8519cc4ae2d0
mwku <168341474+mwku@users.noreply.github.com>
2024-06-05 18:26:21 +0800

    Update main.py
    use embed and soluve bug

--- cmds/main.py
+++ cmds/main.py
@@ -17,10 +17,11 @@ class Main(Cog_Extension):
     @commands.command()
     async def Todolist(self, ctx, action, item=None, time=None):
         '''
-        Todolist add item 新增一個事項到todolist
+        Todolist add item date新增一個事項到todolist
 
         Todolist remove item 從todolist刪除一個事項
                         all 刪除todolist的所有事項
+                        None 刪除todolist的所有事項
 
         Todolist print 印出todolist
 
@@ -28,48 +29,54 @@ class Main(Cog_Extension):
 
         Todolist time 依時間排序todolist
 
-        每個user會依據user id建立不同的todolist(type dict)自己的todolist其他人不能進行刪減、排序或輸出,保戶使用者的todolist
+        每個user會依據user id建立不同的todolist (dict)自己的todolist其他人不能進行刪減、排序或輸出,保護使用者的todolist
         '''
         if ctx.author.id not in self.to_do_list:
             self.to_do_list[ctx.author.id] = {}
+
         if action == 'add':
-            if time != None and len(time.split('/')) == 3:
-                if item not in self.to_do_list and len(str(time.split('/')[0])) == 4 and len(str(time.split('/')[1])) == 2 and len(str(time.split('/')[2])) == 2:
-                    self.to_do_list[ctx.author.id][str(item)] = time.split('/')
-                    await ctx.send('success')
-                elif item not in self.to_do_list:
-                    await ctx.send("Time's forming should be YYYY/MM/DD")
+            if time and len(time.split('/')) == 3:
+                year, month, day = time.split('/')
+                if self.is_valid_date(year, month, day):
+                    if item not in self.to_do_list[ctx.author.id]:
+                        self.to_do_list[ctx.author.id][str(item)] = [
+                            year, month, day]
+                        await ctx.send(embed=discord.Embed(title='success', description=f"{item} is added in Todolist", color=discord.Color.green()))
+                    else:
+                        await ctx.send(embed=discord.Embed(title='error', description=f'{item} had been added', color=discord.Color.red()))
                 else:
-                    await ctx.send(f'{item} had been added')
-            elif time != None:
-                await ctx.send(f'{item} had been added')
+                    await ctx.send(embed=discord.Embed(title='error', description="Date is unavailable", color=discord.Color.red()))
+            elif time:
+                await ctx.send(embed=discord.Embed(title='error', description="Time's forming should be YYYY/MM/DD", color=discord.Color.red()))
             else:
-                await ctx.send('You have to input time')
+                await ctx.send(embed=discord.Embed(title='error', description='You have to input time', color=discord.Color.red()))
+
         elif action == 'remove':
             if item != 'all':
-                if item != None:
+                if item:
                     if item in self.to_do_list[ctx.author.id]:
                         self.to_do_list[ctx.author.id].pop(item)
-                        await ctx.send('success')
+                        await ctx.send(embed=discord.Embed(title='success', description=f'{item} is removed', color=discord.Color.green()))
                     else:
-                        await ctx.send(f'{item} not in to do list')
+                        await ctx.send(embed=discord.Embed(title='error', description=f'{item} not in Todolist', color=discord.Color.red()))
                 else:
                     self.to_do_list[ctx.author.id] = {}
-                    await ctx.send('success')
+                    await ctx.send(embed=discord.Embed(title='success', description=f'Todolist is clear', color=discord.Color.green()))
             else:
                 self.to_do_list[ctx.author.id] = {}
-                await ctx.send('success')
+                await ctx.send(embed=discord.Embed(title='success', description=f'Todolist is clear', color=discord.Color.green()))
+
         elif action == 'print':
             if len(self.to_do_list[ctx.author.id]) == 0:
-                await ctx.send(f"```yaml\nuser name : {ctx.author.name}\n\nnot thing in todolist```")
+                await ctx.send(embed=discord.Embed(title=f'user name : {ctx.author.name}', description=f'```yaml\nNothing in Todolist```', color=discord.Color.green()))
             else:
                 i = 0
                 for k in self.to_do_list[ctx.author.id]:
                     if len(k) > i:
                         i = len(k)
-                result = [f"user name : {ctx.author.name}\n"]
+                result = []
                 k = 'item'
-                v = 'date'
+                v = 'time'
                 while len(k) < i+1:
                     k += ' '
                 result.append(f"{k}: {v}")
@@ -79,29 +86,30 @@ class Main(Cog_Extension):
                     v = '/'.join(v)
                     result.append(f"{k}: '{v}'")
                 result = '\n'.join(result)
-                await ctx.send(f'```yaml\n{result}\n```')
+                await ctx.send(embed=discord.Embed(title=f'user name : {ctx.author.name}', description=f'```yaml\n{result}```', color=discord.Color.green()))
 
         elif action == 'sort':
             self.to_do_list[ctx.author.id] = dict(
                 sorted(self.to_do_list[ctx.author.id].items()))
-            await ctx.send('success')
+            await ctx.send(embed=discord.Embed(title='success', description=f'Todolist is sorted', color=discord.Color.green()))
+
         elif action == 'time':
             sorted_keys = sorted(
                 self.to_do_list[ctx.author.id], key=lambda x: self.sort_by_date(ctx, x))
             self.to_do_list[ctx.author.id] = {
                 key: self.to_do_list[ctx.author.id][key] for key in sorted_keys}
-            await ctx.send('success')
+            await ctx.send(embed=discord.Embed(title='success', description=f'Todolist is sorted by time', color=discord.Color.green()))
 
     def sort_by_date(self, ctx, key):
         year, month, day = map(int, self.to_do_list[ctx.author.id][key])
         return datetime(year, month, day)
 
-    '''
-    TODO
-    Add the necessary bot commands here
-    Consider using data.json to store some data such as url
-    '''
-
+    def is_valid_date(self, year, month, day):
+        try:
+            datetime(int(year), int(month), int(day))
+            return True
+        except ValueError:
+            return False
 
 async def setup(bot):
     await bot.add_cog(Main(bot))
