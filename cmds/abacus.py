910ff699ddad29aac5e8ea97ae678889080343f8
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2024-06-16 12:40:59 +0800

    Update abacus.py
    Timer functionality added

--- cmds/abacus.py
+++ cmds/abacus.py
@@ -1,8 +1,8 @@
 import json
+import time
 from discord.ext import commands
-from typing import List
 import discord
-
+import sqlite3
 from core import Cog_Extension
 from random import randint, shuffle
 
@@ -14,27 +14,44 @@ class Abacus(Cog_Extension):
         self.instances = dict()
 
 
+
     @commands.command()
     async def abacus(self, ctx, abacus: str, level: int):
         user_id = ctx.author.id
         if user_id not in self.instances:
-            self.instances[user_id] = AbacusInstance(abacus, self.context[level])
-            await ctx.send(embed=discord.Embed(title=f"Abacus Level {level}", description = "Use $a [answer] to answer.", color=discord.Color.green()))
-            await ctx.send(embed=discord.Embed(title=1, description = self.instances[user_id].problem_set[0][0].question))
+            if abacus != "abacus" or abacus != "mental":
+                await ctx.send(embed = discord.Embed(title = "格式錯誤", description = "**$abacus [abacus|mental] [level]**"))
+                return
+            else:
+                self.instances[user_id] = AbacusInstance(abacus, self.context[level])
+                await ctx.send(embed=discord.Embed(title=f"Abacus Level {level}", description = "Use $a [answer] to answer.", color=discord.Color.green()))
+                await ctx.send(embed=discord.Embed(title=1, description = self.instances[user_id].problem_set[0][0].question))
+                self.instances[user_id].time = time.time()
         else:
-            await ctx.send("Please finish the current session first.")
+            await ctx.send(embed = discord.Embed(title = "失敗", description = "請先完成當前測驗。"))
     
     @commands.command()
     async def a(self, ctx, answer: int):
         user_id = ctx.author.id 
         if user_id not in self.instances:
-            await ctx.send("Error occured. Use **$abacus [abacus|mental] [level]** to start an instance.")
+             await ctx.send(embed = discord.Embed(title = "失敗", description = "測驗尚未開始. 使用 **$abacus [abacus|mental] [level]** 以開始測驗"))
         else:
             if self.instances[user_id].count < 29:
-                await ctx.send(embed=discord.Embed(title=self.instances[user_id].count + 1, description = self.instances[user_id].next(answer)))
+                desc =  self.instances[user_id].next(answer)
+                await ctx.send(embed=discord.Embed(title=(self.instances[user_id].count + 1), description = desc))
             else:
-                result = self.instances[user_id].finish(answer)
-                await ctx.send(f"{ctx.author.name} 的結果\n分數：{result[1]}/200", embeds = result[0])
+                result = self.instances[user_id].finish(answer, user_id)
+                await ctx.send(f"{ctx.author.name} 的結果\n分數：{result[1]}/200\n總花費時間：{result[2]}分{result[3]}秒", embeds = result[0])
+                self.instances.pop(user_id)
+    @commands.command()
+    async def abacusExit(self, ctx):
+        user_id = ctx.author.id 
+        if user_id not in self.instances:
+            await ctx.send(embed = discord.Embed(title = "失敗", description = "測驗尚未開始. 使用 **$abacus [abacus|mental] [level]** 以開始測驗"))
+        else:
+            self.instances.pop(user_id)
+            await ctx.send(embed = discord.Embed(title="成功", description="成功離開當前測驗。"))
+        
 
 
 
@@ -48,6 +65,8 @@ class AbacusInstance():
         self.problem_set = [[], [], []]
         self.count = 0
         self.answer = []
+        self.category = category
+        self.time = 0.0
         # [0]: +- [1]: * [2]:/
         for _ in range(0, 10):
             for i in range(0, 3):
@@ -56,7 +75,9 @@ class AbacusInstance():
         self.answer.append(answer)
         self.count += 1
         return self.problem_set[self.count // 10][self.count % 10].question
-    def finish(self, answer):
+    def finish(self, answer, user_id):
+        curr_time = time.time()
+        time_spent = int(curr_time - self.time)
         title = ["加減算", "乘算", "除算"]
         embed_list = []
         self.answer.append(answer)
@@ -71,7 +92,27 @@ class AbacusInstance():
                     answer[i][j] = f"**{answer[i][j]}**"
             embed_list[i].add_field(name = "你的答案", value = "\n".join([str(answer[i][m]) for m in range(0, 10)]))
             embed_list[i].add_field(name = "正確答案", value = "\n".join([str(self.problem_set[i][m].answer) for m in range(0, 10)]))
-        return (embed_list, score)
+
+        # update the database
+        """
+        con = sqlite3.connect("user_data.db")
+        cur = con.cursor()
+        res = cur.execute(f"SELECT * FROM abacus_data WHERE user_id = {user_id}")
+        if res.fetchone() == None:
+            if self.category == "abacus":
+                # (user_id, men_count, men_tscore, men_ttime, men_btime, aba_count, aba_tscore, aba_ttime)
+                cur.execute(f"INSERT INTO abacus_data 
+                            VALUES ({user_id}, 0, 0, 0, 0, 1, {score}, {time_spent}, {score});")
+            else:
+                cur.execute(f"INSERT INTO abacus_data 
+                            VALUES ({user_id}, 1, {score}, {time_spent}, {time_spent}, 0, 0, 0, 0);")
+            con.commit()
+            con.close()
+        else:
+            ori_data = res.fetchone()
+        """
+
+        return (embed_list, score, time_spent // 60, time_spent % 60)
         
         
 
